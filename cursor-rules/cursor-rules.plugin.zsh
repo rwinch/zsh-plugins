4ec60f9a05f435a6c93d6eaa5f3c44eaf8b752f4
Robert Winch <362503+rwinch@users.noreply.github.com>
2026-03-17 08:07:43 -0500

    move to function

--- cursor-rules/cursor-rules.plugin.zsh
+++ cursor-rules/cursor-rules.plugin.zsh
@@ -1,9 +1,10 @@
 # Cursor rules plugin: Provides a command to symlink ~/code/rwinch/.cursor/rules 
 # into the current directory's .cursor/rules.
 
-export CURSOR_RULES_SOURCE=~/code/rwinch/.cursor/rules
+
 
 function cursor-rules() {
+  export CURSOR_RULES_SOURCE=~/code/rwinch/.cursor/rules
   if [[ ! -d "$CURSOR_RULES_SOURCE" ]]; then
     echo "cursor-rules: Source directory $CURSOR_RULES_SOURCE does not exist" >&2
     return 1
